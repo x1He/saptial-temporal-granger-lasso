61645dc1faedb10746d81d1b36b554cc6a034708
zjnsteven <jzhao02@email.wm.edu>
2015-06-24 13:53:58 -0400

    add functions

--- run.R
+++ run.R
@@ -5,12 +5,40 @@
 refinedtest = test[,c(4:8,10:21)]
 input = t(refinedtest)
 N = dim(input)[1]
-      
+L = 
 
 #find (lamda,gama) pair min bic
 lamda2set = c(0,0.01,0.1,1,10,100)
 gama_max = 
 gamaset = c(0,0.01*gama_max,0.1*gama_max,gama_max)
+
+lamdasize = length(lamda2set)
+gamasize = length(gamaset)
+#lamda,gamma combination array
+#res = array(NA,c(1,2,1))
+#bic value range 
+#minbic = 0
+cur = 1 
+for(i in 1:lamdasize){
+  for(j in 1:gamasize){
+    betaGL = beta(input,gama,L)
+    dfgl = dfGL(lamda,gama,betam,N,p,X,Y)
+    bicvalue = bic(betaGL, x, y,dfgl)
+    #res[,,cur] = c(lamda2set[i],gamaset[j])
+    #cur = cur + 1
+    if(i==1 && j==1){
+      minbic = bicvalue
+      res = c(lamda2set[i],gamaset[j])
+    }
+    else{
+      minbic = min(minbic,bicvalue)
+      if(minbic == bicvalue){
+        res = c(lamda2set[i],gamaset[j])
+      }
+    }
+  }
+}
+
 
 
 
